23738c9501bc3330d5702498c913cdb2eaacd5f0
NathanSaldanha <nmariano85@gmail.com>
2021-04-04 17:07:04 -0300

    Adicionando teste para modelo tag e ajuste de teste home

--- spec/requests/contents_spec.rb
+++ spec/requests/contents_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "Contents", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/contents/index"
-      expect(response).to have_http_status(:success)
+      get "/contents"
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
