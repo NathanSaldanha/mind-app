727d74317baa7874e865907285abb58774517d54
NathanSaldanha <nmariano85@gmail.com>
2021-04-04 16:18:06 -0300

    Corrigindo dominio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_options = {from: "noreply@herokuapp.com"}
+  config.action_mailer.default_options = {from: "noreply@heroku.com"}
 
   config.action_mailer.delivery_method = :smtp
 
